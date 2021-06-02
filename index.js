bc0db3eaefc7274279943b5fb25910f08b9d9184
Tamara <tamara_almanaseer@live.com>
2021-06-02 16:19:03 +0300

    .env edit

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ const mongoose=require('mongoose');
 mongoose
   .connect(process.env.MONGOOSE_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    server.listen(3000, () => console.log('server up'));
+    server.listen(process.env.PORT, () => console.log('server up'));
   })
   .catch(e => console.error('Could not start server', e.message));
